5a4d5f7809a604d1ff75bff97366626afd3286a3
Sophia Shaikh <sophias930@gmail.com>
2018-12-10 03:17:07 -0500

    basic first page created + logged in

--- project/script.js
+++ project/script.js
@@ -2,14 +2,38 @@ var root = "http://comp426.cs.unc.edu:3001/";
 
 
 $(document).ready(() => {
-    // login button click
-    $('#login').click(function () {
-        let user = $('#username').val();
-        let pass = $('#password').val();
+    checkLogin();
 
-        checkLogin(user, pass);
-
-    });
-
-
+    $('#submit').click(function() {
+        buildBasicFirstPage();
+    })    
 });
+
+function checkLogin() {
+    var data = { user: { username: "sophiass", password: "Foofa123!" }};
+    $.ajax(root + 'sessions',
+        {
+            type: 'POST',
+            xhrFields: { withCredentials: true },
+            dataType: 'json',
+            data: { 
+                user: { 
+                    username: "sophiass", 
+                    password: "Foofa123!" 
+                }
+            },
+            success: (response) => {
+                // debugging purposes
+            },
+            error: () => {
+                alert('Check your connection and try again.');
+            }
+        });
+
+}
+
+function buildBasicFirstPage() {
+    // TODO build this page!!!! 
+        // make api calls and whatever
+    $('body').append("congrats you clicked a button!<br>");
+}
